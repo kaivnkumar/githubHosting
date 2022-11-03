3782d6b4bafc2b318914a0d56e61823ad6623131
kavinkumar <kavinkumars218@gmail.com>
2022-11-03 20:39:47 +0530

    fixed routing issue

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
 import './App.css';
 import FirstPage from "../src/Components/FirstPage";
 import SecondPage from "../src/Components/SecondPage";
@@ -6,12 +6,14 @@ import ThirdPage from "../src/Components/ThirdPage";
 
 function App() {
   return (
-    <div>  
-      <Routes>
-        <Route path="/" element={<FirstPage />} exact></Route>
-        <Route path="/second" element={<SecondPage />} exact></Route>
-        <Route path="/third" element={<ThirdPage />} exact></Route>
-      </Routes>
+    <div>
+      <BrowserRouter basename="/githubHosting">
+        <Routes>
+          <Route path="/" element={<FirstPage />} exact></Route>
+          <Route path="/second" element={<SecondPage />} exact></Route>
+          <Route path="/third" element={<ThirdPage />} exact></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
